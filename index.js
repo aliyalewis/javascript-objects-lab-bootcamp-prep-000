403169b0ab9327a844b466205616398a7e998e9a
Aliya Lewis <aliyalewis5@gmail.com>
2019-04-05 18:34:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ var recipes = {}
 function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign({}, object, {[key]: value})
 }
+
+function destructivelyUpdateOjbectWithKeyAndValue(object, key, value) {
+  return object[key] = value
+  
+}
